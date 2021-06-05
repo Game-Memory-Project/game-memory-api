4d14128c134ab5425652efadbb9b71e3b712538c
laisgbueno7 <laisgbueno62@gmail.com>
2021-06-05 11:26:33 -0300

    projeto inicial

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using game_memo_api.Models;
+
+namespace game_memo_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly ItemsContext _context;
+
+        public ItemsController(ItemsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Items
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Items>>> GetItems()
+        {
+            return await _context.Items.ToListAsync();
+        }
+
+        // GET: api/Items/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Items>> GetItems(long id)
+        {
+            var items = await _context.Items.FindAsync(id);
+
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            return items;
+        }
+
+        // PUT: api/Items/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItems(long id, Items items)
+        {
+            if (id != items.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(items).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Items
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Items>> PostItems(Items items)
+        {
+            _context.Items.Add(items);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetItems), new { id = items.Id }, items);
+        } 
+
+        // DELETE: api/Items/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItems(long id)
+        {
+            var items = await _context.Items.FindAsync(id);
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(items);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ItemsExists(long id)
+        {
+            return _context.Items.Any(e => e.Id == id);
+        }
+    }
+}
